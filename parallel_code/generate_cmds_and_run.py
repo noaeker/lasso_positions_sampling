2d1b5496091560ae4945572d019f25492c0f47f6
noaeker <n12345e@gmail.com>
2020-12-24 12:32:22 +0200

    adding cmds_file

--- parallel_code/generate_cmds_and_run.py
+++ parallel_code/generate_cmds_and_run.py
@@ -1,12 +1,9 @@
-from parallel_code.help_functions import *
-args = ps_parser()
-job_line = f'module load gcc/gcc-8.2.0; module load python/python-anaconda3.6.5-orenavr2!@#python; python /groups/pupko/noaeker/positions_sampling/current_code/MSA_positions_sampling.py --run_prefix {args.run_prefix} --jobs_prefix {args.jobs_prefix} --n_MSAs {args.n_MSAs} --n_jobs {args.n_jobs} --first_msa_ind {args.first_msa_ind} --n_random_starting_trees {args.n_random_starting_trees} --max_n_seq {args.max_n_seq} --only_evaluate_lasso \t{args.jobs_prefix}'
-create_dir_if_not_exists(RESULTS_FOLDER)
-curent_run_results_folder = os.path.join(RESULTS_FOLDER, args.run_prefix)
-create_or_clean_dir(curent_run_results_folder)
-cmds_path =  os.path.join(curent_run_results_folder, args.run_prefix + ".cmds")
-job_log_path = os.path.join(curent_run_results_folder, args.run_prefix + "_tmp_log")
-with open(cmds_path, 'w') as cmds_f:
-    cmds_f.write(job_line)
-if not LOCAL_RUN:
-    os.system(f'/bioseq/bioSequence_scripts_and_constants/q_submitter_power.py {cmds_path} {job_log_path}')
+
+job_line = 'module load gcc/gcc-8.2.0; module load python/python-anaconda3.6.5-orenavr2!@#python; python /groups/pupko/noaeker/positions_sampling/current_code/MSA_positions_sampling.py --run_prefix five_taxa --jobs_prefix five_taxa --n_MSAs 50 --n_jobs 50 --first_msa_ind 0 --n_random_starting_trees 1000 --max_n_seq 5\tfive_taxa'
+
+cmds_path  = "five_taxa.cmds"
+
+with open(cmds_path,'w') as cmds:
+    cmds.write(job_line)
+# if not LOCAL_RUN:
+#     os.system(f'/bioseq/bioSequence_scripts_and_constants/q_submitter_power.py {cmds_path} {job_log_path}')
