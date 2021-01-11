10f6b7b076d76f8f02d16a3a12d9afded8076d59
noaeker <n12345e@gmail.com>
2021-01-11 11:43:18 +0200

    adding a fix

--- parallel_code/combine_current_csv_files.py
+++ parallel_code/combine_current_csv_files.py
@@ -5,5 +5,5 @@ unified_csv_path = '/groups/pupko/noaeker/lasso_positions_sampling_results/curre
 job_folder = '/groups/pupko/noaeker/lasso_positions_sampling_results/brlen_spr'
 
 
-csv_paths= [os.path.join(job_folder,"job_{}".format(i),"{}.csv") for i in range(50)]
+csv_paths= [os.path.join(job_folder,"job_{}".format(i),"{}.csv".format(i)) for i in range(1)]
 all_MSA_results = add_csvs_content(csv_paths, unified_csv_path)
