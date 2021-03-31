7dd39fc4e38c7106dad808c40c524b7ec8d462ec
noaeker <n12345e@gmail.com>
2021-04-01 00:35:28 +0300

    small_fix

--- parallel_code/ps.py
+++ parallel_code/ps.py
@@ -108,7 +108,7 @@ def main():
         logging.info(
             "After removing files that exist in {} there are {} MSAs".format(all_jobs_backup_csv, len(file_path_list)))
     file_path_list = remove_MSAs_with_not_enough_seq(file_path_list, args.min_n_seq)
-    logging.info("There are {} MSAs with at least {} sequences".format(len(file_path_list), args.min_n_se))
+    logging.info("There are {} MSAs with at least {} sequences".format(len(file_path_list), args.min_n_seq))
     file_path_list = file_path_list[args.first_msa_ind:args.first_msa_ind + args.n_MSAs]
     logging.debug("Alignment files are " + str(file_path_list))
     csv_path_to_status_path_dict = distribute_MSAs_over_jobs(file_path_list, all_jobs_results_folder, args)
