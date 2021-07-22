ed8daf1731a78647bb8a8335e771df05e8b7b1bd
noaeker <n12345e@gmail.com>
2021-07-22 14:57:42 +0300

    Adding max pct to one of the options in glmnet

--- R_code/lasso_glmnet.R
+++ R_code/lasso_glmnet.R
@@ -22,9 +22,9 @@ lasso_path_tidy = tidy(lasso_path)
 
 coefs_cnt <- rowid_to_column(lasso_path_coefs_sum_tidy) %>% rename(step = rowid, non_zero_cnt = x)  
 n_loci = ncol(X)
-pcts = c(0.005,0.01,0.05,0.1,0.15)
+pcts = c(0.005,0.01,0.05,0.1,0.15, max(coefs_cnt$non_zero_cnt)/n_loci)
 counts = pcts*n_loci
-relevant_non_zero_cnts<-coefs_cnt %>% crossing(pcts) %>%  mutate(counts = pcts*n_loci,diff = (non_zero_cnt-counts)) %>% filter (diff>=0) %>% group_by(counts, pcts) %>% filter (diff ==min(diff)) %>% pull(non_zero_cnt) 
+relevant_non_zero_cnts<-coefs_cnt %>% crossing(pcts) %>%  mutate(counts = as.integer(pcts*n_loci),diff = (non_zero_cnt-counts)) %>% filter (diff>=0) %>% group_by(counts, pcts) %>% filter (diff ==min(diff)) %>% pull(non_zero_cnt) 
 #lasso_path = glmnet(x = X, y = y, nlambda = 100, alpha =1,lower.limits=0, relax = TRUE)
 output <- lasso_path_tidy %>% inner_join(coefs_cnt) %>% filter (non_zero_cnt %in% relevant_non_zero_cnts) %>% rename(loci = term)
 write_csv(output,paste(str_trim(args[2]),"/r_lasso.csv",sep=""))
