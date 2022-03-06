cd996c2bb636298ea56b03b280d59fdf80cdd2e5
noaeker <n12345e@gmail.com>
2022-03-06 12:27:20 +0200

    smal fix

--- main_code/lasso_model_pipeline.py
+++ main_code/lasso_model_pipeline.py
@@ -15,7 +15,7 @@ def evaluate_lasso_performance_on_test_data(curr_msa_stats, curr_run_directory, 
     if not test_data:
         test_data = curr_msa_stats
     optimized_random_trees_path = test_data.get("optimized_test_topologies_path",test_data.get("test_optimized_trees_path"))
-    if curr_msa_stats["pars_optimized_model"]:
+    if curr_msa_stats.get("pars_optimized_model"):
         edit_num_locis_in_model_file(curr_msa_stats["pars_optimized_model"], n_chosen_locis)
     # logging.info("Evaluating model on test optimized random trees")
     true_ll_values = test_data["test_ll_values"]
